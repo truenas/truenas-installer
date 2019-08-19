e8b2cd1d40a2e9e5f27761470d76ab163fc58cf1
Kris Moore <kris@ixsystems.com>
2019-08-19 14:45:22 -0400

    Add mk-deb.sh helper

---
+++ mk-deb.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -z "$1" ] ; then
+	echo "Missing GPG signing key"
+	exit 1
+fi
+
+DATESTAMP=$(date +%Y%m%d%H%M)
+dch -M -v ${DATESTAMP}~truenas+1 --force-distribution --distribution bullseye-truenas-unstable "Auto Update from Jenkins CI"
+
+dpkg-buildpackage --sign-key=${1} -S -sa
