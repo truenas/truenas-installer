3991ea88e3db2447f23dca77fde9e3ffe228ac39
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-02-04 12:52:50 +0500

    Add basic create cert impl

--- truenas_installer/server/api/truenas_connect/acme.py
+++ truenas_installer/server/api/truenas_connect/acme.py
@@ -1,6 +1,6 @@
 from .acme_utils import normalize_acme_config
 from .cache import tnc_config, update_tnc_config
-from .crypto.cert_utils import get_hostnames_from_hostname_config
+from .cert_utils import generate_csr, get_hostnames_from_hostname_config
 from .hostname import hostname_config, register_update_ips
 from .urls import get_acme_config_url
 from .utils import auth_headers, call, get_account_id_and_system_id
@@ -24,3 +24,19 @@ async def acme_config() -> dict:
         resp = normalize_acme_config(resp)
 
     return resp | {'tnc_configured': True}
+
+
+async def create_cert():
+    await register_update_ips()
+    tnc_hostname_config = await hostname_config()
+    if tnc_hostname_config['error']:
+        raise Exception(f'Failed to fetch TN Connect hostname config: {tnc_hostname_config["error"]}')
+
+    tnc_acme_config = await acme_config()
+    if tnc_acme_config['error']:
+        raise Exception(f'Failed to fetch TN Connect ACME config: {tnc_acme_config["error"]}')
+
+    hostnames = get_hostnames_from_hostname_config(tnc_hostname_config)
+    csr, private_key = generate_csr(hostnames)
+    dns_mapping = {f'DNS:{hostname}': hostname for hostname in hostnames}
+
