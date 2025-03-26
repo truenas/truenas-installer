47f96b83bf315881b633d86028b3d40b5bb36abe
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-03-27 01:50:11 +0500

    Add a cache file to retrieve tnc config

---
+++ truenas_installer/server/api/truenas_connect/cache.py
@@ -0,0 +1,41 @@
+import json
+
+
+CACHE_FILE = '/tmp/tnc.cache'
+
+
+def default_tnc_config() -> dict:
+    return {
+        'jwt_token': None,
+        'registration_details': {},
+        'ips': [],
+        'csr_public_key': None,
+        'certificate_public_key': None,
+        'certificate_private_key': None,
+        'account_service_base_url': 'https://account-service.dev.ixsystems.net/',
+        'leca_service_base_url': 'https://leca-server.dev.ixsystems.net/',
+        'tnc_base_url': 'https://truenas.connect.dev.ixsystems.net/',
+        'claim_token': None,
+        'initialization_in_progress': False,
+        'initialization_completed': False,
+        'initialization_error': None,
+        'system_id': None,
+        'truenas_version': None,
+        'enabled': False,
+    }
+
+
+def tnc_config() -> dict:
+    try:
+        with open(CACHE_FILE, 'r') as f:
+            return json.loads(f.read())
+    except FileNotFoundError:
+        return default_tnc_config()
+
+
+def update_tnc_config(data: dict) -> dict:
+    config = tnc_config() | data
+    with open(CACHE_FILE, 'w') as f:
+        f.write(json.dumps(config))
+
+    return config
