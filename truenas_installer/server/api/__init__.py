0aba15cb20350e5555fe318cf860b65be3dbd109
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-03-27 01:48:43 +0500

    Add api basic file

--- truenas_installer/server/api/__init__.py
+++ truenas_installer/server/api/__init__.py
@@ -2,3 +2,4 @@ import truenas_installer.server.api.adoption  # noqa
 import truenas_installer.server.api.info  # noqa
 import truenas_installer.server.api.install  # noqa
 import truenas_installer.server.api.power  # noqa
+from truenas_installer.server.api.truenas_connect.api import * # noqa
