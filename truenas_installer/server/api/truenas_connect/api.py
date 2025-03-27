0cef07043c1d0e2a383b2226024c42e52efa83d7
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-03-27 06:23:03 +0500

    Add endpoint to configure tnc

--- truenas_installer/server/api/truenas_connect/api.py
+++ truenas_installer/server/api/truenas_connect/api.py
@@ -1,10 +1,18 @@
+import errno
+import time
+import uuid
+
+from truenas_installer.server.error import Error
 from truenas_installer.server.method import method
 
-from .cache import get_tnc_config
+from .cache import get_tnc_config, update_tnc_config
 from .schema import TNC_CONFIG_SCHEMA
 
 
-__all__ = ['tnc_config']
+__all__ = ['tnc_config', 'configure_tnc']
+
+
+CONFIGURED_TNC: bool = False
 
 
 @method(None, TNC_CONFIG_SCHEMA)
@@ -12,4 +20,53 @@ async def tnc_config(context):
     """
     Get TrueNAS Connect configuration.
     """
+    return get_tnc_config()
+
+
+@method({
+    'type': 'object',
+    'properties': {
+        'ips': {
+            'type': 'array',
+            'items': {
+                'type': 'string',  # FIXME: Validate this to be proper IP Addresses
+            },
+        },
+        'enabled': {'type': 'boolean'},
+        'account_service_base_url': {'type': 'string'},
+        'leca_service_base_url': {'type': 'string'},
+        'tnc_base_url': {'type': 'string'},
+    },
+    'required': ['enabled', 'ips'],
+}, TNC_CONFIG_SCHEMA)
+async def configure_tnc(context, data):
+    """
+    Enable and configure TrueNAS Connect.
+    """
+    global CONFIGURED_TNC
+    if CONFIGURED_TNC is True:
+        raise Error('Configuration can only be updated once', errno.EINVAL)
+
+    if data['enabled'] and not data['ips']:
+        raise Error('No IP addresses provided', errno.EINVAL)
+
+    if data['enabled']:
+        CONFIGURED_TNC = True
+
+    config = get_tnc_config() | data
+    claim_token_generated = False
+    if config['enabled']:
+        # Let's generate claim token
+        config.update({
+            'claim_token': str(uuid.uuid4()),
+            'claim_token_expiration': time.time() + (45 * 60),  # 45 min expiration
+            'system_id': str(uuid.uuid4()),
+            'truenas_version': context.server.installer.version,
+            'initialization_in_progress': True,
+        })
+        claim_token_generated = True
+
+    update_tnc_config(config | data)
+    # TODO: Let's kick of the registration process
+
     return get_tnc_config()
